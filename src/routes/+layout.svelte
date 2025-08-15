2b2d174683b6df14a22ad282ec03cf81d657518c
Tang Hao Liang <tang.hao.liang.thl@gmail.com>
2025-08-16 00:59:11 +0800

    Testing routing from main page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
 	import '../app.css';
+	import { base } from '$app/paths';
 
 	let { children } = $props();
 </script>
+
+<nav>
+	<a href="{base}/blog">Homepage</a>
+</nav>
 
 {@render children()}
