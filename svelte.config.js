faaf25b163b95b796551a6d1361fa1fefd83996d
Tang Hao Liang <53548686+nobodyishappy@users.noreply.github.com>
2025-08-27 13:42:50 +0800

    Revert "Attempt deploy to vercel"
    This reverts commit 2433f47533c48b31235e6bfc9731dbd916d95547.

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
 const config = {
 	kit: {
 		adapter: adapter({
-			
+			fallback: '404.html'
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
